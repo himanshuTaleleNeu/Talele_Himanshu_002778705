6020cac12a3e3c54d6654aa3f5a0e740328ddf76
Himanshu Talele <talele.h@northeastern.edu>
2022-10-01 09:00:01 +0530

    AED Assignment 1 commit

---
+++ Aed_Assign_1/src/model/EmpOperations.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Himanshu
+ */
+public class EmpOperations {
+    
+    private ArrayList<Employee>  empCrudOpertions;
+      
+    public EmpOperations()
+    {
+           this.empCrudOpertions = new ArrayList<Employee>();
+           
+    }
+
+    public ArrayList<Employee> getEmpCrudOpertions() {
+        return empCrudOpertions;
+    }
+
+    public void setEmpCrudOpertions(ArrayList<Employee> empCrudOpertions) {
+        this.empCrudOpertions = empCrudOpertions;
+    }
+
+    public  Employee addNewRecord()
+    {
+        Employee newRecord = new Employee();
+        empCrudOpertions.add(newRecord);
+        return newRecord;
+    }
+           
+}
---
+++ Aed_Assign_1/src/model/Employee.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Himanshu
+ */
+public class Employee {
+    private String name;
+    private String empId;
+    private String age;
+    private String gender;
+    private String startDate;
+    private String level;
+    private String teamInfo;
+    private String posTitle;
+    private String cellNum;
+    private String emailAdd; 
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosTitle() {
+        return posTitle;
+    }
+
+    public void setPosTitle(String posTitle) {
+        this.posTitle = posTitle;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getCellNum() {
+        return cellNum;
+    }
+
+    public void setCellNum(String cellNum) {
+        this.cellNum = cellNum;
+    }
+
+    public String getEmailAdd() {
+        return emailAdd;
+    }
+
+    public void setEmailAdd(String emailAdd) {
+        this.emailAdd = emailAdd;
+    }
+    
+}
